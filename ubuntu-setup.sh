a0ce534e32535f8ffc3991f1dbe22ac57d59018d
Janix <janix@duck.com>
2025-05-04 10:19:26 +0200

    Add KeePassXC and Cryptomator to the setup script

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -22,51 +22,41 @@ sudo apt install -y \
     qbittorrent \
     filezilla \
     inkscape \
+    gimp \
+    gnome-tweaks \
     zsh \
     npm \
-    nodejs
+    nodejs \
+    dosbox \
+    retroarch \
+    keepassxc
 
 # Ensure Snap daemon is running
 sudo systemctl enable --now snapd
 
-# Install Snap applications
-sudo snap install \
-    brave \
-    notesnook \
-    element-desktop \
-    rustdesk \
-    superproductivity \
-    steam \
-    dbeaver-ce \
-    postman \
-    code --classic \
-    spotify \
-    slack --classic \
-    discord
+# Install Snap applications (one by one to avoid errors)
+sudo snap install brave
+sudo snap install notesnook
+sudo snap install element-desktop
+sudo snap install rustdesk
+sudo snap install superproductivity
+sudo snap install steam
+sudo snap install dbeaver-ce
+sudo snap install postman
+sudo snap install code --classic
+sudo snap install spotify
+sudo snap install slack --classic
+sudo snap install discord
+sudo snap install obs-studio
+sudo snap install signal-desktop
+sudo snap install session-desktop
+sudo snap install telegram-desktop
+sudo snap install cryptomator
 
-# Install GNOME Shell Extensions
-echo "Installing GNOME Shell Extensions..."
-EXTENSIONS=(
-    "https://extensions.gnome.org/extension-data/user-themegnome-shell-extensions.gcampax.github.com.v42.shell-extension.zip"
-    "https://extensions.gnome.org/extension-data/dash-to-dockgnome-shell-extensions.gcampax.github.com.v72.shell-extension.zip"
-    "https://extensions.gnome.org/extension-data/weather-oclockjenslody.de.v27.shell-extension.zip"
-)
-EXTENSIONS_DIR="$HOME/.local/share/gnome-shell/extensions"
-
-mkdir -p "$EXTENSIONS_DIR"
-
-for EXTENSION_URL in "${EXTENSIONS[@]}"; do
-    EXTENSION_ZIP=$(basename "$EXTENSION_URL")
-    wget -O "/tmp/$EXTENSION_ZIP" "$EXTENSION_URL"
-    unzip -o "/tmp/$EXTENSION_ZIP" -d "$EXTENSIONS_DIR"
-    rm "/tmp/$EXTENSION_ZIP"
-done
-
-# Restart GNOME Shell to apply extensions (requires user confirmation)
-echo "GNOME Shell extensions installed. Please restart GNOME Shell to apply changes."
-
-# Install developer tools
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-curl -fsSL https://zed.dev/install.sh | sh
+# Install Google Chrome
+echo "Installing Google Chrome..."
+wget -q -O /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y /tmp/google-chrome-stable_current_amd64.deb
+rm /tmp/google-chrome-stable_current_amd64.deb
 
 echo "Installation complete! A restart is recommended."
